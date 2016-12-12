7e7179d798af73338931e2781df9fd7029d89eae
Simon Acosta <acosta1204@gmail.com>
2016-12-12 17:37:14 +0100

    Added the navbar to all webpage except application html

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,8 @@
 class Order < ApplicationRecord
 	belongs_to :user
 
-	validates :source, presence: true
-	validates_numericality_of :amount
-
-protected
-
-	def validate
-		errors.add(:amount, "should be a positive value") if amount.nil? || amount < 0.01
-	end
+	validates :source, :presence => {:message => " Location cannot be blank"}
+	validates :amount, :numericality => { :greater_than => 0, :less_than_or_equal_to => 200, :message => "Maximum amount $200" }, :presence => true
 
 
 end

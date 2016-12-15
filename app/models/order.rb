2d0678009ab6a58f80c7b17c936ec2cce5e538b6
Simon Acosta <acosta1204@gmail.com>
2016-12-15 13:13:06 +0100

    Final debugging

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
 	belongs_to :user
 
-	validates :source, :presence true
+	validates :source, presence: true
 	validates :amount, :numericality => { :greater_than => 0, :less_than_or_equal_to => 200}, :presence => true
 
 
